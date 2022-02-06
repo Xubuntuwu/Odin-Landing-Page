81efb53c44ae93f8f0ad6fc08df61a3b21227550
Xubuntuwu <uwuxubuntu@gmail.com>
2022-02-06 17:52:33 +0100

    capitalize first letter of names from prompts

--- javascript.js
+++ javascript.js
@@ -2,11 +2,15 @@ window.onload=function(){
 
     const buttonpress = document.querySelector('.sign-up');
     const normalbuttton = document.querySelector('.normalpage');
-
+    
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+      
         function greet() {
             const userName = prompt('What is your name?');
             if (userName != null) {
-            alert(`Welcome ${userName}, nice to meet you!`);
+            alert(`Welcome ${capitalizeFirstLetter(userName)}, nice to meet you!`);
             alert('You turn my world upside down!');
             document.querySelector('body').style.transform = "rotate(180deg)";
             }
@@ -14,7 +18,7 @@ window.onload=function(){
         function normalgreet() {
             const name = prompt('What was your name again?');
             if (name != null) {
-                alert(`Welcome back ${name}, you found the right button!`);
+                alert(`Welcome back ${capitalizeFirstLetter(name)}, you found the right button!`);
                 alert('You turn my world rightside up!');
                 document.querySelector('body').style.transform = "rotate(0deg)";
             }
